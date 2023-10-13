f0c3a6c8c00346b1c4a7322517ea7e1d16d14432
angrycarp <khmg525@naver.com>
2023-10-13 15:19:54 +0900

    작업시작해야되는데2121
    1

---
+++ FinalProject/src/main/java/kr/kh/finalproject/controller/ShopController.java
@@ -0,0 +1,19 @@
+package kr.kh.finalproject.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ShopController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String write(HttpSession session, HttpServletRequest request) throws Exception {
+	
+		return "shop/search";
+	}
+	
+}
