aa864f361625f613b5252bb742347890cd9da0ba
Maycowducky <maycowduck@kakao.com>
2023-10-16 12:24:27 +0900

    수정

--- FinalProject/src/main/java/kr/kh/finalproject/controller/HomeController.java
+++ FinalProject/src/main/java/kr/kh/finalproject/controller/HomeController.java
@@ -9,23 +9,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-<<<<<<< HEAD
-=======
+
 /**
  * Handles requests for the application home page.
  */
->>>>>>> main
+
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-<<<<<<< HEAD
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String write(HttpSession session, HttpServletRequest request) throws Exception {
-	
-		return "/index";
-=======
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -33,7 +26,6 @@ public class HomeController {
 	public String write(HttpSession session, HttpServletRequest request) throws Exception {
 	
 		return "/main/index";
->>>>>>> main
 	}
 	
 }
