4efc9c455fcc18fcfb3f211914c96387fccd743e
Leehaegyou <134986451+Leehaegyou@users.noreply.github.com>
2023-10-23 15:32:30 +0900

    2023-10-23 15:32

--- FinalProject/src/main/java/kr/kh/finalproject/controller/MemberController.java
+++ FinalProject/src/main/java/kr/kh/finalproject/controller/MemberController.java
@@ -1,21 +1,15 @@
 package kr.kh.finalproject.controller;
 
-
-import java.util.HashMap;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-
 public class MemberController {
-
-	@RequestMapping(value="/member/signup", method=RequestMethod.GET)
+	
+	@RequestMapping(value = "/member/signup", method = RequestMethod.GET)
 	public String signup() {
-		
+
 		return "/member/signup";
 	}
-
 }
