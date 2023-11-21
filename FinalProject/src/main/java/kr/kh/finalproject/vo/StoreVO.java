f569e6d9163d4ad64e39290c23a636a4344ecdc7
오제 <heyoje325@gmail.com>
2023-11-21 17:40:51 +0900

    coffee : test 관리자페이지 매장등록(승인)
    시간포맷 Getter추가
    public String getBo_up_date_str() {
    	if(bo_up_date == null) {
    		return "";
    	}
    	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
    	return format.format(bo_up_date);
    }
    
    public String getBo_reg_date_str() {
    	if(bo_reg_date == null) {
    		return "";
    	}
    	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
    	return format.format(bo_reg_date);
    }

--- FinalProject/src/main/java/kr/kh/finalproject/vo/StoreVO.java
+++ FinalProject/src/main/java/kr/kh/finalproject/vo/StoreVO.java
@@ -1,5 +1,6 @@
 package kr.kh.finalproject.vo;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -16,9 +17,9 @@ public class StoreVO {
 	
 	String st_open; // 매장오픈여부. 아마 안쓸듯.
 	
-	// @DateTimeFormat(pattern = 24시간 형식)으로 Setter를 만들어줘야함!  
+	// @DateTimeFormat(pattern = 24시간 형식)으로 'Setter'를 만들어줘야함!  
 	// => "09:30" 같은 문자열이 들어온다면, 어노테이션을 통해 해당 문자열을 'Date' 객체로 변환할 때 "HH:mm"형식을 따르도록 함.
-	@DateTimeFormat(pattern = "HH:mm")
+	@DateTimeFormat(pattern = "HH:mm") // 세터를 만들어주는것
 	Date st_opentime; // 영업시작시간
 	@DateTimeFormat(pattern = "HH:mm")
 	Date st_closetime; // 영업종료시간
@@ -28,5 +29,25 @@ public class StoreVO {
 	int st_approval;
 	// 매장명 - 사업자회원 테이블과 JOIN해서 가져옴
 	String st_store_name; // 사업자회원에 있는 bm_store_name을 st_store_name으로 vo에 저장
+	String st_bm_num; // 사업자번호 bm - bm_num
+	
+	// rngdate
+	// 게터를 바꿔줘야함
+	
+	/** 시간정보 화면에 나타내주는 getter 메소드 추가 */
+	public String getSt_opentime_str() {
+		if(st_opentime == null) {
+			return "";
+		}
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+		return format.format(st_opentime);
+	}
+	public String getSt_closetime_str() {
+		if(st_closetime == null) {
+			return "";
+		}
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+		return format.format(st_closetime);
+	}
 	 
 }
